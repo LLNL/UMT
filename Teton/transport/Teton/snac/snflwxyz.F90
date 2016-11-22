7a281f676358392476c8a2cc7ade5067f1b6b0ca
David Appelhans <dappelh@us.ibm.com>
2016-11-22 15:28:38 -0500

    All arrays now passed to sweep directly instead of SoA

--- Teton/transport/Teton/snac/snflwxyz.F90
+++ Teton/transport/Teton/snac/snflwxyz.F90
@@ -14,7 +14,7 @@
 !***********************************************************************
 
 ! if batches are not currently size of bins, data is staged wrong.
-#define BATCHSIZE 90
+#define BATCHSIZE 96
    subroutine snflwxyz(ipath, PSIB, PSI, PHI, angleLoopTime)
 
    use, intrinsic :: iso_c_binding
@@ -22,6 +22,8 @@
    use constant_mod
    use Size_mod
    use Quadrature_mod
+   use Geometry_mod
+   use ZoneData_mod
    use snswp3d_mod
    use cudafor
 
@@ -55,8 +57,14 @@
    real(adqt)       :: maxFluxError
    real(adqt)       :: startOMPLoopTime, endOMPLoopTime, theOMPLoopTime
 
+   ! zero copy pointers for phi and psib
    type(C_DEVPTR)                    :: d_phi_p
-   real(adqt), dimension(:,:), device, allocatable :: d_phi(:,:)
+   type(C_DEVPTR)                    :: d_psib_p
+   real(adqt), device, allocatable :: d_phi(:,:)
+   real(adqt), device, allocatable :: d_psib(:,:,:)
+
+   ! picking up environment variables
+   character(len=255) :: envstring
 
    real(adqt), device :: d_psi(QuadSet%Groups,Size%ncornr,BATCHSIZE)
    type(C_PTR) :: cptr
@@ -67,21 +75,7 @@
 
    integer :: devnum, cacheconfig
 
-!  Function
-
-   ! This sets up to allow zero copy use of phi directly on the device:
-   ! Get a device pointer for phi, put it to d_phi_p
-   istat = cudaHostGetDevicePointer(d_phi_p, C_LOC(phi(1,1)), 0)
-   ! Translate that C pointer to the fortran array with given dimensions
-   call c_f_pointer(d_phi_p, d_phi, [QuadSet%Groups,Size%ncornr] )
-   
-
-   theOMPLoopTime=0.0
-
-
-    
-
-!  Mesh Constants
+!  Convenient Mesh Constants
 
    Groups = QuadSet%Groups
 
@@ -91,6 +85,43 @@
    NangBin = maxval(QuadSet%NangBinList(:))
    NumBin = QuadSet%NumBin
    call mpi_comm_rank(mpi_comm_world, myrank, info)
+
+
+
+   ! This sets up to allow zero copy use of phi directly on the device:
+   ! Get a device pointer for phi, put it to d_phi_p
+   istat = cudaHostGetDevicePointer(d_phi_p, C_LOC(phi(1,1)), 0)
+   ! Translate that C pointer to the fortran array with given dimensions
+   call c_f_pointer(d_phi_p, d_phi, [QuadSet%Groups,Size%ncornr] )
+   
+
+   ! This sets up to allow zero copy use of phi directly on the device:
+   ! Get a device pointer for phi, put it to d_phi_p
+   istat = cudaHostGetDevicePointer(d_phi_p, C_LOC(phi(1,1)), 0)
+   ! Translate that C pointer to the fortran array with given dimensions
+   call c_f_pointer(d_phi_p, d_phi, [QuadSet%Groups,Size%ncornr] )
+
+   !call get_environment_variable ("ZEROCOPY", envstring )
+
+
+   if(1) then
+   !if(TRIM(envstring) .eq. "True") then
+      print *, "Zero copy setup of d_psib"
+      ! Get a device pointer for psib, put it to d_psib_p
+      istat = cudaHostGetDevicePointer(d_psib_p, C_LOC(psib(1,1,1)), 0)
+      ! Translate that C pointer to the fortran array with given dimensions
+      call c_f_pointer(d_psib_p, d_psib, [QuadSet%Groups,Size%nbelem,QuadSet%NumAngles] )
+      ! conversion from c to f not really need anymore with cuda c call.
+   else !NOT WORKING YET.
+      ! explicitly batch d_psib onto GPU. 
+      print *, "explicitly batching d_psib onto GPU."
+      ! Allocate an array to hold subset of angles:
+      allocate(d_psib(Groups,nbelem,BATCHSIZE))
+   endif
+
+   theOMPLoopTime=0.0
+
+   call mpi_comm_rank(mpi_comm_world, myrank, info)
    
    ! Double check the rank and divice mapping:
    !istat = cudaGetDevice(devnum)
@@ -98,7 +129,7 @@
 
 
    !if (myrank .eq. 0) write(0,*) ' groups, ncornr, nbelem, angles, NangBin, NumBin = ', groups, ncornr, nbelem, angles, NangBin, NumBin
-
+   
    ! Set the Cache configuration for the GPU (use more L1, less shared)
    istat = cudaDeviceGetCacheConfig(cacheconfig)
    print *, cacheconfig
@@ -183,10 +214,41 @@
 !        boundary flux array PSIB is also updated here.
 !        Mesh cycles are fixed automatically.
 
-         call snswp3d(anglebatch, QuadSet%AngleOrder(mm1,binSend), &
-                      QuadSet%d_AngleOrder(mm1,binSend),           &
-                      QuadSet%d_next,QuadSet%d_nextZ,              &
-                      QuadSet%d_passZstart, d_psi, psib, stream(binRecv))
+          call snswp3d(     anglebatch,                     &
+                              Size%nzones,               &
+                              QuadSet%Groups,            &
+                              Size%ncornr,               &
+                              QuadSet%NumAngles,         &
+                              QuadSet%d_AngleOrder(mm1,binSend),        & ! only need angle batch portion
+                              Size%maxCorner,            &
+                              Size%maxcf,                &
+                              binRecv,                   &
+                              NangBin,                   &
+                              Size%nbelem,                &
+                              QuadSet%d_omega,             &
+                              Geom%ZDataSoA%nCorner,                &
+                              Geom%ZDataSoA%nCFaces,                &
+                              Geom%ZDataSoA%c0,                &
+                              Geom%ZDataSoA%A_fp,                &
+                              Geom%ZDataSoA%A_ez,                &
+                              Geom%ZDataSoA%Connect,             &
+                              Geom%ZDataSoA%STotal,              &
+                              Geom%ZDataSoA%STime,               &
+                              Geom%ZDataSoA%Volume,             &
+                              d_psi,                      &  ! only want angle batch portion
+                              d_psib,                      &
+                              QuadSet%d_next,              &
+                              QuadSet%d_nextZ,             &
+                              Geom%ZDataSoA%Sigt,                &
+                              Geom%ZDataSoA%SigtInv,             &
+                              QuadSet%d_passZstart,              &
+                              stream(binRecv))
+
+
+         ! call snswp3d(anglebatch, QuadSet%AngleOrder(mm1,binSend), &
+         !              QuadSet%d_AngleOrder(mm1,binSend),           &
+         !              QuadSet%d_next,QuadSet%d_nextZ,              &
+         !              QuadSet%d_passZstart, d_psi, psib, stream(binRecv))
 
 
          ! update psi on the host:
--- Teton/transport/Teton/snac/snswp3d.F90
+++ Teton/transport/Teton/snac/snswp3d.F90
@@ -25,13 +25,294 @@ module snswp3d_mod
 
 contains
 
+  attributes(global) subroutine GPU_sweep(anglebatch,                     &
+                              nzones,               &
+                              Groups,            &
+                              ncornr,               &
+                              NumAngles,         &
+                              Angles,        & ! only angle batch portion
+                              maxCorner,            &
+                              maxcf,                &
+                              binRecv,                   &
+                              NangBin,                   &
+                              nbelem,                &
+                              omega,             &
+                              nCornerArray,                &
+                              nCFacesArray,                &
+                              c0Array,                &
+                              A_fp,                &
+                              A_ez,                &
+                              Connect,             &
+                              STotal,              &
+                              STime,               &
+                              Volume,             &
+                              psicbatch,                      &  ! only angle batch portion
+                              psib,                      &
+                              next,              &
+                              nextZ,             &
+                              SigtArray,                &
+ !                             SigtInv,             &
+                              passZstart             )
+    implicit none
+
+    !  Arguments
+
+   integer, value,    intent(in)    :: anglebatch
+   integer, value,    intent(in)    :: nzones
+   integer, value,    intent(in)    :: Groups
+   integer, value,    intent(in)    :: ncornr
+   integer, value,    intent(in)    :: NumAngles
+
+   integer,    device, intent(in) :: Angles(anglebatch)
+
+   integer, value,    intent(in)    :: maxCorner
+   integer, value,    intent(in)    :: maxcf
+   integer, value,    intent(in)    :: binRecv
+   integer, value,    intent(in)    :: NangBin
+   integer, value,    intent(in)    :: nbelem
+   real(adqt), device, intent(in)    :: omega(3,NumAngles)
+   integer, device, intent(in):: nCornerArray(nzones)
+   integer, device, intent(in):: nCFacesArray(nzones)
+   integer, device, intent(in):: c0Array(nzones)
+   real(adqt), device, intent(in)    :: A_fp(3,maxcf,maxCorner,nzones) !ndim,maxcf,maxCorner,nzones
+   real(adqt), device, intent(in)    :: A_ez(3,maxcf,maxCorner,nzones) !ndim,maxcf,maxCorner,nzones
+   integer, device, intent(in):: Connect(3,maxcf,maxCorner,nzones) 
+   real(adqt), device, intent(in)    :: STotal(Groups, maxCorner, nzones)
+   real(adqt), device, intent(in)    :: STime(Groups, ncornr, NumAngles)
+   real(adqt), device, intent(in)    :: Volume(maxCorner, nzones)
+   real(adqt), device, intent(inout) :: psicbatch(Groups,ncornr,anglebatch)
+   real(adqt), device, intent(inout) :: psib(Groups,nbelem,NumAngles)
+   integer,    device, intent(in) :: next(ncornr+1,NumAngles)
+   integer,    device, intent(in) :: nextZ(nzones,NumAngles)
+   real(adqt), device, intent(in)    :: SigtArray(Groups, nzones)
+!   real(adqt), device, intent(in)    :: SigtInv(Groups, nzones)
+   integer,    device, intent(in) :: passZstart(nzones,NumAngles)   
+
+
+    !  Local Variables
+
+    integer    :: Angle, i, ib, ic, icfp, icface, id, ifp, ig, k, nxez
+    integer    :: zone, c, cez, ii, mm, ndone
+    integer    :: p, ndoneZ, passZcount
+    integer    :: nCorner, nCFaces, c0
+
+    !!FIXME: sizes are hardcoded at present due to a CUDA Fortran limitation
+    !real(adqt), shared :: Volume(8,NZONEPAR) ! (maxCorner)
+    !real(adqt), shared :: A_fp(3,3,NZONEPAR) ! (ndim,maxcf)
+    !real(adqt), shared :: A_ez(3,3,NZONEPAR) ! (ndim,maxcf)
+    !integer,    shared :: Connect(3,3,NZONEPAR) ! (3,maxcf)
+
+    integer    :: ez_exit(3) ! (maxcf)
+
+    real(adqt) :: fouralpha, fouralpha4, aez, aez2, area_opp, psi_opp
+    real(adqt) :: source, sigv, sigv2, gnum, gtau, sez, sumArea
+    real(adqt) :: Sigt, SigtInv
+
+    real(adqt) :: src(8)      ! (maxCorner)
+    real(adqt) :: Q(8)        ! (maxCorner)
+    real(adqt) :: afpm(3)     ! (maxcf)
+    real(adqt) :: coefpsic(3) ! (maxcf)
+    real(adqt) :: psifp(3)    ! (maxCorner)
+    real(adqt) :: tpsic(8)    ! (maxCorner)
+
+    !  Constants
+
+    parameter (fouralpha=1.82d0)
+    parameter (fouralpha4=5.82d0)
+
+
+    ig = threadIdx%x
+    mm = blockIdx%z
+    Angle = Angles(mm)
+
+    p = 0
+    ndoneZ = 0
+    PassLoop: do while (ndoneZ < nzones)
+       p = p + 1
+       passZcount = passZstart(p+1,Angle) - passZstart(p,Angle)
+       
+       !if(ig .le. Groups) then
+          ZoneLoop: do ii=threadIdx%z,passZcount,blockDim%z
+
+             !!FIXME: simplifying assumption that all zones have same nCorner values
+             !! (they're all 8 from what we've seen). If this isn't true in general,
+             !! just convert this into a table lookup
+             ndone = (ndoneZ+ii-1) * maxCorner
+
+             zone = nextZ(ndoneZ+ii,Angle)
+
+             nCorner = nCornerArray(zone)
+             nCFaces =   nCFacesArray(zone)
+             c0      =   c0Array(zone)
+             Sigt    =  SigtArray(ig,zone)
+             SigtInv = one/Sigt !need to thread?
+
+             !  Contributions from volume terms
+
+             do c=1,nCorner
+                !do ig= threadIdx%x, Groups, blockDim%x
+                source     =  STotal(ig,c,zone) +  STime(ig,c0+c,Angle)
+                !enddo
+                Q(c)       = SigtInv*source 
+                src(c)     =  Volume(c,zone)*source
+             enddo
+
+             CornerLoop: do i=1,nCorner
+
+                ic      = next(ndone+i,Angle)
+                c       = ic - c0
+
+                sigv    = Sigt* Volume(c,zone)
+
+                !  Calculate Area_CornerFace dot Omega to determine the 
+                !  contributions from incident fluxes across external 
+                !  corner faces (FP faces)
+
+                sumArea = zero
+
+                do icface=1,ncfaces
+
+                   afpm(icface) = omega(1,Angle)* A_fp(1,icface,c,zone) + &
+                        omega(2,Angle)* A_fp(2,icface,c,zone) + &
+                        omega(3,Angle)* A_fp(3,icface,c,zone)
+
+                   icfp    =  Connect(1,icface,c,zone)
+                   ib      =  Connect(2,icface,c,zone)
+
+                   if ( afpm(icface) >= zero ) then
+                      sumArea = sumArea + afpm(icface)
+                   else
+                      if (icfp == 0) then
+                         psifp(icface) = psib(ig,ib,Angle)
+                      else
+                         psifp(icface) = psicbatch(ig,icfp,mm)
+                      endif
+
+                      src(c) = src(c) - afpm(icface)*psifp(icface)
+                   endif
+                enddo
+
+                !  Contributions from interior corner faces (EZ faces)
+
+                nxez = 0
+
+                do icface=1,nCFaces
+
+                   aez = omega(1,Angle)* A_ez(1,icface,c,zone) + &
+                        omega(2,Angle)* A_ez(2,icface,c,zone) + &
+                        omega(3,Angle)* A_ez(3,icface,c,zone) 
+
+                   if (aez > zero ) then
+
+                      sumArea        = sumArea + aez
+                      area_opp       = zero
+                      nxez           = nxez + 1
+                      cez            = Connect(3,icface,c,zone)
+                      ez_exit(nxez)  = cez
+                      coefpsic(nxez) = aez
+
+                      if (nCFaces == 3) then
+
+                         ifp = mod(icface,nCFaces) + 1
+
+                         if ( afpm(ifp) < zero ) then
+                            area_opp   = -afpm(ifp)
+                            psi_opp    =  psifp(ifp)
+                         endif
+
+                      else
+
+                         ifp        = icface
+                         area_opp   = zero
+                         psi_opp    = zero
+
+                         do k=1,nCFaces-2
+                            ifp = mod(ifp,nCFaces) + 1
+                            if ( afpm(ifp) < zero ) then
+                               area_opp   = area_opp   - afpm(ifp)
+                               psi_opp    = psi_opp    - afpm(ifp)*psifp(ifp)
+                            endif
+                         enddo
+
+                         psi_opp = psi_opp/area_opp
+
+                      endif
+
+                      TestOppositeFace: if (area_opp > zero) then
+
+                         aez2 = aez*aez
+
+                         sigv2        = sigv*sigv
+                         gnum         = aez2*( fouralpha*sigv2 +              &
+                              aez*(four*sigv + three*aez) )
+
+                         gtau         = gnum/                                    &
+                              ( gnum + four*sigv2*sigv2 + aez*sigv*(six*sigv2 + &
+                              two*aez*(two*sigv + aez)) ) 
+
+                         sez          = gtau*sigv*( psi_opp - Q(c) ) +   &
+                              half*aez*(one - gtau)*( Q(c) - Q(cez) )
+                         src(c)       = src(c)   + sez
+                         src(cez)     = src(cez) - sez
+
+                      else
+
+                         sez          = half*aez*( Q(c) - Q(cez) )
+                         src(c)       = src(c)   + sez
+                         src(cez)     = src(cez) - sez
+
+                      endif TestOppositeFace
+
+                   endif
+
+                enddo
+
+                !  Corner angular flux
+
+                tpsic(c) = src(c)/(sumArea + sigv)
+
+                !  Calculate the angular flux exiting all "FP" surfaces
+                !  and the current exiting all "EZ" surfaces.
+                !  The downstream corner index is "ez_exit."
+
+                !  Zone Interior or "EZ" Faces
+
+                do icface=1,nxez
+                   cez      = ez_exit(icface)
+                   src(cez) = src(cez) + coefpsic(icface)*tpsic(c)
+                enddo
+
+             enddo CornerLoop
+
+             !  Copy temporary flux into the global array
+
+             !print *, "ig, c0, Angle", ig, c0, Angle
+             do c=1,nCorner
+                psicbatch(ig,c0+c,mm) = tpsic(c)
+                !if(ig>Groups .or. c0+c > ncornr .or. Angle > NumAngles) then
+                !   print *, "ig, c0, c, Angle", ig, c0, c, Angle
+                !endif
+                !psic(ig,c0+c,Angle) = tpsic(c)
+                !psic(ig,c0+c,Angle) = tpsic(c)
+             enddo
+
+          enddo ZoneLoop
+       !endif ! ig .le. groups
+
+       ndoneZ = ndoneZ + passZcount
+
+       call syncthreads
+
+    enddo PassLoop
+
+  end subroutine GPU_sweep
 
 
 
 
 
 
-  attributes(global) subroutine GPU_sweep(Groups, NumAngles, anglebatch, &
+  attributes(global) subroutine GPU_sweep_old(Groups, NumAngles, anglebatch, &
        ncornr, nzones, nbelem, &
        maxcf, maxCorner, passZstart, Angles, omega, &
        ZDataSoA, next, nextZ, psicbatch, psib)
@@ -271,7 +552,7 @@ contains
 
     enddo PassLoop
 
-  end subroutine GPU_sweep
+  end subroutine GPU_sweep_old
 
 
 
@@ -829,9 +1110,35 @@ end subroutine setExitFlux
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   subroutine snswp3d(anglebatch, Angles, d_Angles,  &
-                      next, nextZ, passZstart, psicbatch, PSIB, streamid)
-
+   subroutine snswp3d(     anglebatch,                     &
+                              nzones,               &
+                              Groups,            &
+                              ncornr,               &
+                              NumAngles,         &
+                              d_Angles,        & ! only angle batch portion
+                              maxCorner,            &
+                              maxcf,                &
+                              binRecv,                   &
+                              NangBin,                   &
+                              nbelem,                &
+                              d_omega,             &
+                              d_nCorner,                &
+                              d_nCFaces,                &
+                              d_c0,                &
+                              d_A_fp,                &
+                              d_A_ez,                &
+                              d_Connect,             &
+                              d_STotal,              &
+                              d_STime,               &
+                              d_Volume,             &
+                              d_psicbatch,                      &  ! only angle batch portion
+                              d_psib,                      &
+                              d_next,              &
+                              d_nextZ,             &
+                              d_Sigt,                &
+                              d_SigtInv,             &
+                              d_passZstart,              &
+                              streamid)
 
    use kind_mod
    use constant_mod
@@ -845,20 +1152,39 @@ end subroutine setExitFlux
    implicit none
 
 !  Arguments
+   integer, parameter :: ndim=3
 
    integer,    intent(in)    :: anglebatch
+   integer,    intent(in)    :: nzones
+   integer,    intent(in)    :: Groups
+   integer,    intent(in)    :: ncornr
+   integer,    intent(in)    :: NumAngles
 
-   integer,    intent(in) :: Angles(anglebatch)
    integer,    device, intent(in) :: d_Angles(anglebatch)
-   integer,    device, intent(in) :: next(Size%ncornr+1,QuadSet%NumAngles)
-   integer,    device, intent(in) :: nextZ(Size%nzones,QuadSet%NumAngles)
-   integer,    device, intent(in) :: passZstart(Size%nzones,QuadSet%NumAngles)
 
-   real(adqt), device, intent(inout) :: psicbatch(QuadSet%Groups,Size%ncornr,anglebatch)
-
-   real(adqt), intent(inout) :: psib(QuadSet%Groups,Size%nbelem,QuadSet%NumAngles)
-
-   integer(kind=cuda_stream_kind), intent(in) :: streamid
+   integer,    intent(in)    :: maxCorner
+   integer,    intent(in)    :: maxcf
+   integer,    intent(in)    :: binRecv
+   integer,    intent(in)    :: NangBin
+   integer,    intent(in)    :: nbelem
+   real(adqt), device, intent(in)    :: d_omega(3,NumAngles)
+   integer, device, intent(in):: d_nCorner(nzones)
+   integer, device, intent(in):: d_nCFaces(nzones)
+   integer, device, intent(in):: d_c0(nzones)
+   real(adqt), device, intent(in)    :: d_A_fp(ndim,maxcf,maxCorner,nzones) !ndim,maxcf,maxCorner,nzones
+   real(adqt), device, intent(in)    :: d_A_ez(ndim,maxcf,maxCorner,nzones) !ndim,maxcf,maxCorner,nzones
+   integer, device, intent(in):: d_Connect(3,maxcf,maxCorner,nzones) 
+   real(adqt), device, intent(in)    :: d_STotal(Groups, maxCorner, nzones)
+   real(adqt), device, intent(in)    :: d_STime(Groups, ncornr, NumAngles)
+   real(adqt), device, intent(in)    :: d_Volume(maxCorner, nzones)
+   real(adqt), device, intent(inout) :: d_psicbatch(QuadSet%Groups,Size%ncornr,anglebatch)
+   real(adqt), device, intent(inout) :: d_psib(QuadSet%Groups,Size%nbelem,QuadSet%NumAngles)
+   integer,    device, intent(in) :: d_next(Size%ncornr+1,QuadSet%NumAngles)
+   integer,    device, intent(in) :: d_nextZ(Size%nzones,QuadSet%NumAngles)
+   real(adqt), device, intent(in)    :: d_Sigt(Groups, nzones)
+   real(adqt), device, intent(in)    :: d_SigtInv(Groups, nzones)
+   integer,    device, intent(in) :: d_passZstart(Size%nzones,QuadSet%NumAngles)   
+   integer(kind=cuda_stream_kind), intent(in) :: streamid   
 
 !  Local Variables
 
@@ -866,9 +1192,6 @@ end subroutine setExitFlux
 
    type(dim3) :: threads,blocks
 
-   type(C_DEVPTR)                    :: d_psib_p
-   type(C_DEVPTR)                    :: d_psic_p
-   real(adqt), dimension(:,:,:), device, allocatable :: d_psib(:,:,:)
 
 #ifdef PROFILING_ON
    integer profiler(2) / 0, 0 /
@@ -881,39 +1204,39 @@ end subroutine setExitFlux
    call TAU_PROFILE_START(profiler)
 #endif
 
-   istat = cudaHostGetDevicePointer(d_psib_p, C_LOC(psib(1,1,1)), 0)
-
-   call c_f_pointer(d_psib_p, d_psib, [QuadSet%Groups,Size%nbelem,QuadSet%NumAngles] )
-
-
    ! groups*NZONEPAR must be .le. 1024 on K80 hardware
    !threads=dim3(QuadSet%Groups,NZONEPAR,1) 
    threads=dim3(THREADX,1,NZONEPAR) 
    blocks=dim3(1,1,anglebatch)
 
-   ! call newsweep<<<blocks,threads,0,streamid>>>(QuadSet%Groups, QuadSet%NumAngles,       &
-   !                                anglebatch, Size%ncornr, Size%nzones,    &
-   !                                Size%nbelem, Size%maxcf, Size%maxCorner, &
-   !                                passZstart, d_Angles, QuadSet%d_omega,   &
-   !                                Geom%d_ZData, Geom%d_ZDataSoA,           &
-   !                                next, nextZ, d_psic, psiccache, d_psib)
-
-
-
-
-
-
-
-
-   call GPU_sweep<<<blocks,threads,0,streamid>>>(QuadSet%Groups, QuadSet%NumAngles,       &
-                                  anglebatch, Size%ncornr, Size%nzones,    &
-                                  Size%nbelem, Size%maxcf, Size%maxCorner, &
-                                  passZstart, d_Angles, QuadSet%d_omega,   &
-                                  Geom%d_ZDataSoA,           &
-                                  next, nextZ, psicbatch, d_psib)
-
-
-
+   call GPU_sweep<<<blocks,threads,0,streamid>>>( anglebatch,                     &
+                              nzones,               &
+                              Groups,            &
+                              ncornr,               &
+                              NumAngles,         &
+                              d_Angles,        & ! only angle batch portion
+                              maxCorner,            &
+                              maxcf,                &
+                              binRecv,                   &
+                              NangBin,                   &
+                              nbelem,                &
+                              d_omega,             &
+                              d_nCorner,                &
+                              d_nCFaces,                &
+                              d_c0,                &
+                              d_A_fp,                &
+                              d_A_ez,                &
+                              d_Connect,             &
+                              d_STotal,              &
+                              d_STime,               &
+                              d_Volume,             &
+                              d_psicbatch,                      &  ! only angle batch portion
+                              d_psib,                      &
+                              d_next,              &
+                              d_nextZ,             &
+                              d_Sigt,                &
+!                              d_SigtInv,             &
+                              d_passZstart             )
 
 
 
